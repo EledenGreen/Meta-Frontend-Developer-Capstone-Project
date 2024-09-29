2eb1915a53ba6c48dffb27c56b8beb03e8f0bccb
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-09-29 13:36:55 +0530

    unit test updated

--- my-app/src/test/unitTest_1.test.js
+++ my-app/src/test/unitTest_1.test.js
@@ -4,25 +4,47 @@ import { initializeTimes } from '../components/BookingPage'
 import { updateTimes } from '../components/BookingPage'
 
 test('Renders the BookingForm heading', () => {
-  render(<BookingForm />)
+  const availableTimes = ['17:00', '18:00']
+  const setTime = jest.fn()
+  const dispatch = jest.fn()
+  const submitForm = jest.fn()
+
+  render(
+    <BookingForm
+      availableTimes={availableTimes}
+      setTime={setTime}
+      dispatch={dispatch}
+      submitForm={submitForm}
+    />
+  )
   const headingElement = screen.getByText('Booking')
   expect(headingElement).toBeInTheDocument()
 })
 
 test('reducer initialized values', () => {
-  const expectedTimes = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
-
   const result = initializeTimes()
 
-  expect(result).toEqual(expectedTimes)
+  expect(result.times).not.toHaveLength(0)
 })
 
 test('updateTimes return value', () => {
-  const initialState = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
+  const initialState = [
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00',
+    '23:00',
+  ]
+  const preSelectedDate = new Date('2024-09-29')
 
-  const action = { type: 'UPDATE_TIMES' }
+  const action = {
+    type: 'UPDATE_TIMES',
+    payload: preSelectedDate,
+  }
 
   const newState = updateTimes(initialState, action)
 
-  expect(newState).toBe(initialState)
+  expect(newState.times).not.toEqual([])
 })
