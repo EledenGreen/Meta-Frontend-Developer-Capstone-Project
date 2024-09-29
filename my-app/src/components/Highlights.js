4af6e91d47fc3cddbbea77eecaad881aec0e752c
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-09-29 21:41:05 +0530

    button navigated

--- my-app/src/components/Highlights.js
+++ my-app/src/components/Highlights.js
@@ -1,14 +1,18 @@
 import greekSalad from '../images/greek salad.jpg'
 import bruschetta from '../images/bruchetta.jpg'
 import lemon from '../images/lemon dessert.jpg'
+import { useNavigate } from 'react-router-dom'
 
 const Highlights = () => {
+  const navigate = useNavigate()
   return (
     <>
       <section className="specials">
         <div className="specials-header">
           <h2>Specials</h2>
-          <button className="btn online-menu">Online Menu</button>
+          <button onClick={() => navigate('/menu')} className="btn online-menu">
+            Online Menu
+          </button>
         </div>
 
         <div className="specials-grid">
@@ -17,21 +21,27 @@ const Highlights = () => {
             <h3>Greek salad</h3>
             <p>$12.99</p>
             <p>The famous greek salad of lettucces</p>
-            <button className="btn">Order Delivery</button>
+            <button onClick={() => navigate('/order')} className="btn">
+              Order Delivery
+            </button>
           </div>
           <div className="specials-item">
             <img src={bruschetta} alt="bruschetta" />
             <h3>Bruschetta</h3>
             <p>$5.99</p>
             <p>The famous Bruschetta</p>
-            <button className="btn">Order Delivery</button>
+            <button onClick={() => navigate('/order')} className="btn">
+              Order Delivery
+            </button>
           </div>
           <div className="specials-item">
             <img src={lemon} alt="Lemon Dessert" />
             <h3>Lemon Dessert</h3>
             <p>$15.00</p>
             <p>The famous Lemon Dessert</p>
-            <button className="btn">Order Delivery</button>
+            <button onClick={() => navigate('/order')} className="btn">
+              Order Delivery
+            </button>
           </div>
         </div>
       </section>
